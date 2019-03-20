3ff30bf9235dd08377e6678c7a6d4192b4c5fff1
Stan Dewan <sdewan@earthlink.net>
2019-03-20 08:08:25 -0700

    Converted to Android Studio project

---
+++ app/build/generated/source/buildConfig/debug/com/tsop/android/triplePlayPoker/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tsop.android.triplePlayPoker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.tsop.android.triplePlayPoker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.6";
+}
